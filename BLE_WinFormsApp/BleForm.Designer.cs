eddf7192d161210ad0bba1f3822bc51b3f719750
Hans Naert <u0081767@vives.be>
2022-12-06 11:35:01 +0100

    add scan functionality

--- BLE_WinFormsApp/BleForm.Designer.cs
+++ BLE_WinFormsApp/BleForm.Designer.cs
@@ -28,28 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.scanButton = new System.Windows.Forms.Button();
+            this.connectButton = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // button1
+            // scanButton
             // 
-            this.button1.Location = new System.Drawing.Point(67, 70);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.scanButton.Location = new System.Drawing.Point(67, 70);
+            this.scanButton.Name = "scanButton";
+            this.scanButton.Size = new System.Drawing.Size(94, 29);
+            this.scanButton.TabIndex = 0;
+            this.scanButton.Text = "SCAN";
+            this.scanButton.UseVisualStyleBackColor = true;
+            this.scanButton.Click += new System.EventHandler(this.scanButton_Click);
             // 
-            // button2
+            // connectButton
             // 
-            this.button2.Location = new System.Drawing.Point(67, 149);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 29);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.connectButton.Location = new System.Drawing.Point(67, 149);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(94, 29);
+            this.connectButton.TabIndex = 1;
+            this.connectButton.Text = "CONNECT";
+            this.connectButton.UseVisualStyleBackColor = true;
             // 
             // listBox1
             // 
@@ -60,24 +61,24 @@
             this.listBox1.Size = new System.Drawing.Size(572, 404);
             this.listBox1.TabIndex = 2;
             // 
-            // Form1
+            // BleForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.scanButton);
+            this.Name = "BleForm";
+            this.Text = "BLE App";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private Button button1;
-        private Button button2;
+        private Button scanButton;
+        private Button connectButton;
         private ListBox listBox1;
     }
 }
--- BLE_WinFormsApp/BleForm.cs
+++ BLE_WinFormsApp/BleForm.cs
@@ -1,10 +1,117 @@
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.UI.Core;
+
 namespace BLE_WinFormsApp
 {
     public partial class BleForm : Form
     {
+
+        DeviceWatcher deviceWatcher;
+        DeviceInformation deviceInformation;
+        BluetoothLEDevice bluetoothLeDevice;
+        GattDeviceService service;
+        GattCharacteristic characteristic;
+
         public BleForm()
         {
             InitializeComponent();
         }
+        private void scanButton_Click(object sender, EventArgs e)
+        {
+            // Query for extra properties you want returned
+            string[] requestedProperties = { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected" };
+
+            deviceWatcher =
+                         DeviceInformation.CreateWatcher(
+                                 BluetoothLEDevice.GetDeviceSelectorFromPairingState(false),
+                                 null,
+                                 DeviceInformationKind.AssociationEndpoint);
+
+            // Register event handlers before starting the watcher.
+            // Added, Updated and Removed are required to get all nearby devices
+            deviceWatcher.Added += DeviceWatcher_Added;
+            deviceWatcher.Updated += DeviceWatcher_Updated;
+            deviceWatcher.Removed += DeviceWatcher_Removed;
+
+            // EnumerationCompleted and Stopped are optional to implement.
+            //deviceWatcher.EnumerationCompleted += DeviceWatcher_EnumerationCompleted;
+            //deviceWatcher.Stopped += DeviceWatcher_Stopped;
+
+            //deviceWatcher.Received += DeviceWatcher_Received;
+
+            // Start the watcher.
+            deviceWatcher.Start();
+        }
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DeviceWatcher_Removed(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void DeviceWatcher_Updated(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation args)
+        {
+            Invoke(() => listBox1.Items.Add(new DeviceInformationListItem(args)));
+        }
+    }
+
+    public class DeviceInformationListItem
+    {
+        public DeviceInformation deviceInformation;
+
+        public DeviceInformationListItem(DeviceInformation deviceInformation)
+        {
+            this.deviceInformation = deviceInformation;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {deviceInformation.Name} Id:{deviceInformation.Id}";
+        }
+
+    }
+
+    public class GattDeviceServiceListItem
+    {
+        public GattDeviceService service;
+
+        public GattDeviceServiceListItem(GattDeviceService service)
+        {
+            this.service = service;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Service UUID: {service.Uuid}";
+        }
+
+    }
+
+
+    public class GattCharacteristicListItem
+    {
+        public GattCharacteristic characteristic;
+
+        public GattCharacteristicListItem(GattCharacteristic characteristic)
+        {
+            this.characteristic = characteristic;
+        }
+
+        public override string ToString()
+        {
+            return $"Characteristic UUID: {characteristic.Uuid}";
+        }
+
     }
 }
